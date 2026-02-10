fc5e41e6deb6f1c85c8ffb64ed5b459a9c21e2a7
TOTON DHIBAR <127105435+Toton-dhibar@users.noreply.github.com>
2026-02-10 17:57:25 +0530

    Update proxy.js

--- edge-functions/proxy.js
+++ edge-functions/proxy.js
@@ -1,6 +1,6 @@
 export default async (request, context) => {
   const url = new URL(request.url);
-  const targetUrl = "https://zz.sdbuild.me:444" + url.pathname + url.search;
+  const targetUrl = "https://zz.sdbuild.me:443" + url.pathname + url.search;
 
   const backendResponse = await fetch(targetUrl, {
     method: request.method,
